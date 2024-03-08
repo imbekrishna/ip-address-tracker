5b128b465b080dae7c665addd044c955621eab0c
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-03-08 15:23:42 +0530

    added leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import "leaflet/dist/leaflet.css";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import arrowIcon from "./assets/images/icon-arrow.svg";
 
 function App() {
   return (
-    <>
+    <main>
       <div className="info-container">
         <h1 className="title">IP Address Tracker</h1>
         <div className="ip-input-div">
@@ -33,7 +36,23 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+      <MapContainer
+        id="map"
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </main>
   );
 }
 
