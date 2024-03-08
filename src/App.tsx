2965e94c32824bb13fc8a29732781cfae21cf514
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-03-08 15:03:04 +0530

    inital styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import arrowIcon from "./assets/images/icon-arrow.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="info-container">
+        <h1 className="title">IP Address Tracker</h1>
+        <div className="ip-input-div">
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+          />
+          <button className="btn">
+            <img src={arrowIcon} alt="" />
+          </button>
+        </div>
+        <div className="ip-info-div">
+          <div className="ip-info-container">
+            <span>IP Address</span>
+            <p>192.212.174.101</p>
+          </div>
+          <div className="ip-info-container">
+            <span>Location</span>
+            <p>Brooklyn, NY 10001</p>
+          </div>
+          <div className="ip-info-container">
+            <span>Timezone</span>
+            <p>UTC -05:00</p>
+          </div>
+          <div className="ip-info-container">
+            <span>ISP</span>
+            <p>SpaceX Starlink</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
